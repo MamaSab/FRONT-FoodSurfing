9f8d9b5651345c9d7a81fa233a65bd182385f607
SabApprenante <SabApprenante>
2018-09-11 11:42:26 +0200

    mise en place Login

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="app">
  <!-- <img src="./assets/img/13.jpg"> -->
-    <router-view/>
+    <!-- <router-link><router-view/> -->
+      <div id="nav">
+          <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+      </div>
+    <router-view @authenticated="setAuthenticated" />    
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+            return {
+                authenticated: false,
+                mockAccount: {
+                    username: "nraboy",
+                    password: "password"
+                }
+            }
+        },
+        mounted() {
+            if(!this.authenticated) {
+                this.$router.replace({ name: "login" });
+            }
+        },
+        methods: {
+            setAuthenticated(status) {
+                this.authenticated = status;
+            },
+            logout() {
+                this.authenticated = false;
+            }
+        }
 };
 </script>
 
@@ -18,6 +44,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
 
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  width: 1024px;
+  margin: auto;
 }
+
+  body {
+        background-color: #F0F0F0;
+    }
+    h1 {
+        padding: 0;
+        margin-top: 0;
+    }
+    
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,33 @@ import HelloWorld from '@/components/HelloWorld';
 import Organisation from '@/components/Organisation';
 import Participation from '@/components/Participation';
 import Utilisateurs from '@/components/Utilisateurs';
+import Login from "@/components/login.vue";
+import Secure from "@/components/secure.vue";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Utilisateurs',
+    //   component: Utilisateurs,
+    // },
     {
       path: '/',
-      name: 'Utilisateurs',
-      component: Utilisateurs,
+      redirect: {
+          name: "login"
+      }
+  },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/secure",
+      name: "secure",
+      component: Secure,
     },
     {
       path: '/helloWorld',
