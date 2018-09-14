2a6c0df73f8272fa2b77814426cdb176e3f06bb5
SabApprenante <SabApprenante>
2018-09-14 09:51:40 +0200

    LoginAxios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,9 +6,12 @@
         <button type="button" v-on:click="login()">Login</button>
     </div>
 </template>
-
+<!--if(!empty($_SESSION['error'])) {
+    echo $_SESSION['error'];
+} -->
 <script>
-    export default {
+import axios from "axios";
+    export default { 
         name: 'Login',
         data() {
             return {
@@ -21,14 +24,24 @@
         methods: {
             login() {
                 if(this.form.nom != "" && this.form.mot_de_passe != "") {
-                    if(this.form.nom == this.$parent.mockAccount.nom && this.form.mot_de_passe == this.$parent.mockAccount.mot_de_passe) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "HelloWorld" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
+                    axios.post(`http://localhost:8000/login`, this.form)
+                        .then(response => {
+                            if (response.data === 'ok') {
+                                      this.$emit("authenticated", true);
+                                      this.$router.replace({ name: "HelloWorld" });
+                            }
+                            else {
+                                alert("The username and / or password is incorrect");
+                            }
+                        })
+                        
+                    //         if(this.form.nom == this.$parent.mockAccount.nom && this.form.mot_de_passe == this.$parent.mockAccount.mot_de_passe) {
+                  
+                    // } else {
+                    //     
+                    // }
                 } else {
-                    console.log("A username and password must be present");
+                    alert("A username and password must be present");
                 }
             }
         }
